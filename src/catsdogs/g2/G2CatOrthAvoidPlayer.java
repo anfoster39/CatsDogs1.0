1fee5707b16ec5404b8b42d1f141f4e9e00c354d
Anne Foster <AnFoster@seas.upenn.edu>
2013-06-05 13:35:59 -0400

    Anne's version of getting best cat move

--- src/catsdogs/g2/G2CatOrthAvoidPlayer.java
+++ src/catsdogs/g2/G2CatOrthAvoidPlayer.java
@@ -2,6 +2,7 @@ package catsdogs.g2;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.TreeMap;
 
@@ -26,38 +27,93 @@ public class G2CatOrthAvoidPlayer extends catsdogs.sim.Player {
 	}
 	public Move doMove(int[][] board) {
 		ArrayList<PossibleMove> moves = Cat.allLegalMoves(board);
-		PossibleMove which = getMoveWithLowestOrth(moves);
-		while(true){
-			if(!Dog.wins(which.getBoard())){
-				return which;
-			}else{
-				moves.remove(which);
-				 which = getMoveWithLowestOrth(moves);
+		return getBestCatMove(board);
+		
+	}
+	
+	/**
+	 * Takes the current board and finds the best cat move based on our scoring
+	 * @param currentBoard
+	 * @return
+	 */
+	public PossibleMove getBestCatMove(int[][] currentBoard){
+		//looks at each option 
+		ArrayList<PossibleMove> moves = Cat.allLegalMoves(currentBoard);
+		//HashMap<PossibleMove, Integer> scores = new HashMap<PossibleMove, Integer>();
+		PossibleMove bestMove = null;
+		int bestScore = 1000;
+		for (PossibleMove move: moves){
+			if (getFutureScore(move) < bestScore){
+				bestMove = move;
 			}
 		}
-		
+		return bestMove;
 	}
-	public PossibleMove getBestDogSingleMove(int[][] board){
-		
-		return null;
-	}
-	public ArrayList<PossibleMove> dogBestMoves(ArrayList<PossibleMove> moves){
-		ArrayList<PossibleMove> dogMoves = new ArrayList<PossibleMove>();
-		for(PossibleMove pm : moves){
-			dogMoves.add(getBestDogSingleMove(pm.getBoard()));
+	
+	
+	/**
+	 * @param move 
+	 * @return
+	 */
+	private Integer getFutureScore(PossibleMove move) {
+		PossibleMove bestDogMove = getBestDogSingleMove(Dog.allLegalMoves(move.getBoard()));
+		ArrayList<PossibleMove> nextCatMoves = Cat.allLegalMoves(bestDogMove.getBoard());
+		int best = 1000;
+		for (PossibleMove catMove: nextCatMoves){
+			int score = score(catMove);
+			if (score < best){
+				best = score; 
+			}
 		}
-		
-		return dogMoves;
-		
-	}
-	public PossibleMove returnBestMove(ArrayList<PossibleMove> dogMoves){
-		PossibleMove best;
-		for(PossibleMove pm : dogMoves){
-			
-		}
-		
 		return best;
 	}
+
+	/**
+	 * Scores the board for the cat
+	 * @param catMove
+	 * @return
+	 */
+	private int score(PossibleMove catMove) {
+		int score = Cat.allLegalMoves(catMove.getBoard()).size();
+		if(Dog.wins(catMove.getBoard())){
+			score += 1000;
+		}
+		return score;
+	}
+
+	public PossibleMove getBestDogSingleMove(ArrayList<PossibleMove> moves){
+		  Random r = new Random();
+
+		int size = moves.size();
+		int which = r.nextInt(size);
+		PossibleMove firstM = moves.get(which);
+		ArrayList<PossibleMove> secondOptions = Dog.allLegalMoves(firstM.getBoard());
+		size = secondOptions.size();
+		which = r.nextInt(size);
+		PossibleMove retMove = secondOptions.get(which);
+		return retMove;
+	}
+	
+	
+//	public ArrayList<PossibleMove> dogBestMoves(ArrayList<PossibleMove> moves){
+//		ArrayList<PossibleMove> dogMoves = new ArrayList<PossibleMove>();
+//		for(PossibleMove pm : moves){
+//			dogMoves.add(getBestDogSingleMove(Dog.allLegalMoves(pm.getBoard())));
+//		}
+//		
+//		return dogMoves;
+//		
+//	}
+//	public PossibleMove returnBestMove(ArrayList<PossibleMove> dogMoves){
+//		//pruning dogs moves down to best dog move
+//		PossibleMove best;
+//		for(PossibleMove pm : dogMoves){
+//			
+//		}
+//		
+//		return best;
+//	}
+	
 	public PossibleMove getMoveWithLowestOrth(ArrayList<PossibleMove> moves){
 		int minOrth = 100000;
 		PossibleMove which = moves.get(0);
