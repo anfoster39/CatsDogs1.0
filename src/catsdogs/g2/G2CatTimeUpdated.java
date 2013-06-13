087d8be968ca6b81c03cc26d25c2362d3a6c34c2
Anne Foster <AnFoster@seas.upenn.edu>
2013-06-13 14:33:28 -0400

    Playing around with the time

---
+++ src/catsdogs/g2/G2CatTimeUpdated.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package catsdogs.g2;
+
+/**
+ * @author Anne
+ *
+ */
+public class G2CatTimeUpdated {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
